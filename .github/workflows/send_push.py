ab146789b4c3621a11588329fc872916b4660755
gabrielbay011 <gabriel.santos@baymetrics.com.br>
2025-07-25 13:54:10 -0300

    Create send_push.py

---
+++ .github/workflows/send_push.py
@@ -0,0 +1,15 @@
+import requests
+
+API_URL = "127.0.0.1:8000/update"
+
+payload = {
+    "message": "Código atualizado!"
+}
+
+try:
+    response = requests.post(API_URL, json=payload)
+    response.raise_for_status()
+    print(f"Sucesso: {response.status_code}")
+except requests.exceptions.RequestException as e:
+    print(f"Erro ao enviar push para API: {e}")
+    exit(1)
